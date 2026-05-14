797748158ee4dc0fcfd8c9acb4ba8f4b5aca7e04
boosireddyprasadreddy7 <boosireddyprasadreddy@gmail.com>
2026-05-14 08:51:10 +0530

    Implemented protected routes and logout system

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,18 +1,47 @@
+"use client"
+
 import Navbar from "@/components/layout/Navbar"
 
+import useProtectedRoute from "@/hooks/useProtectedRoute"
+
+import { Button } from "@/components/ui/button"
+
+import { useRouter } from "next/navigation"
+
 export default function DashboardPage() {
+  useProtectedRoute()
+
+  const router = useRouter()
+
+  const handleLogout = () => {
+    localStorage.removeItem("token")
+
+    router.push("/login")
+  }
+
   return (
     <main className="min-h-screen bg-gray-100">
       <Navbar />
 
       <section className="p-10">
-        <h1 className="text-5xl font-bold">
-          Dashboard
-        </h1>
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-5xl font-bold">
+              Dashboard
+            </h1>
 
-        <p className="mt-4 text-gray-600 text-lg">
-          Welcome to your AI legal workspace.
-        </p>
+            <p className="mt-4 text-gray-600 text-lg">
+              Welcome to your AI legal workspace.
+            </p>
+          </div>
+
+          <Button
+            variant="destructive"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </div>
       </section>
     </main>
   )
--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import Link from "next/link"
+
 import { Button } from "@/components/ui/button"
 
 export default function Navbar() {
+  const token =
+    typeof window !== "undefined"
+      ? localStorage.getItem("token")
+      : null
+
   return (
     <nav className="w-full bg-white shadow-md px-8 py-4 flex justify-between items-center">
       <Link href="/">
@@ -11,17 +19,28 @@ export default function Navbar() {
       </Link>
 
       <div className="flex gap-4">
-        <Link href="/login">
-          <Button variant="outline">
-            Login
-          </Button>
-        </Link>
+        {token ? (
+          <Link href="/dashboard">
+            <Button>
+              Dashboard
+            </Button>
+          </Link>
 
-        <Link href="/register">
-          <Button>
-            Register
-          </Button>
-        </Link>
+        ) : (
+          <>
+            <Link href="/login">
+              <Button variant="outline">
+                Login
+              </Button>
+            </Link>
+
+            <Link href="/register">
+              <Button>
+                Register
+              </Button>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   )
